bcaee0df5c0e7ad2a5f787e0e890d62e44473567
prasadpc <146842190+prasadpc@users.noreply.github.com>
2025-01-31 12:12:05 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://prasad.mybookshop.shop";
+const API_BASE_URL = "http://dualstack.af802a26abdd346e09a905f3987515ff-788667165.us-east-1.elb.amazonaws.com";
 export default API_BASE_URL;
