ef5030b97b9535f0a7a53f33f5b7f2b684ff219b
prasadpc <146842190+prasadpc@users.noreply.github.com>
2025-01-31 12:36:21 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://a331591e73930415eb6850d0e7365b41-257023302.us-east-1.elb.amazonaws.com";
+const API_BASE_URL = "http://aa467f750078a43f1a87a6fb8fb77ac8-614511141.us-east-1.elb.amazonaws.com";
 export default API_BASE_URL;
