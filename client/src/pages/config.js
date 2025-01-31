e0c68b8ce6ac51731949fe368122f3380f8db969
prasadpc <146842190+prasadpc@users.noreply.github.com>
2025-01-31 12:23:11 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://dualstack.af802a26abdd346e09a905f3987515ff-788667165.us-east-1.elb.amazonaws.com";
+const API_BASE_URL = "http://a331591e73930415eb6850d0e7365b41-257023302.us-east-1.elb.amazonaws.com";
 export default API_BASE_URL;
